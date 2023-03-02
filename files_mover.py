1deceb1ac3a7df454ac7fa3397006b4a6b112c7b
arisuvade <ariesdave253@gmail.com>
2023-03-02 11:09:11 +0800

    added music dir

--- files_mover.py
+++ files_mover.py
@@ -8,6 +8,7 @@ dir_path: str = "/home/aries/Downloads/"
 
 docs_path: str = "/home/aries/Documents/"
 pics_path: str = "/home/aries/Pictures/"
+music_path: str = "/home/aries/Music/"
 
 files = os.listdir(dir_path)
 
@@ -21,3 +22,7 @@ for file in files:
     elif file.endswith(".pdf") or file.endswith(".docx"):
         print(file, "moved to Documents.")
         shutil.move(file_path, docs_path)
+
+    elif file.endswith(".mp3"):
+        print(file, "moved to Music.")
+        shutil.move(file_path, music_path)
