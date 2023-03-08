b43d92a6c6b36590043fecfe881c597b56a4b690
arisuvade <ariesdave253@gmail.com>
2023-03-08 22:58:30 +0800

    first commit

---
+++ automate_touchpad.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+id=$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')
+
+if [[ $id -ne 13 ]]; then
+    xinput set-prop "$id" "Device Enabled" 0
+else
+    xinput set-prop "$id" "Device Enabled" 1
+fi
