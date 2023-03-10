09b0d3c3dd6b64600ceffae950fed90e3b3fcbe1
arisuvade <ariesdave253@gmail.com>
2023-03-10 20:51:29 +0800

    added libinput command

--- touchpad_on.sh
+++ touchpad_on.sh
@@ -2,6 +2,10 @@
 
 # On the touchpad
 synclient TouchpadOff=0
+
+id=$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')
+xinput set-prop "$id" "Device Enabled" 0
+
 echo "Touchpad on"
 
 # On the tap-to-click
