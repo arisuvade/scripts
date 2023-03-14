0887bcd98a5ec97c09d1e25411d23c4f02ecb1c4
arisuvade <ariesdave253@gmail.com>
2023-03-14 21:15:02 +0800

    added touchscreen to turn off

--- automate_touchpad.sh
+++ automate_touchpad.sh
@@ -4,6 +4,11 @@ id=$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')
 
 if [[ $id -ne 13 ]]; then
     xinput set-prop "$id" "Device Enabled" 0
+
+    # Disable touchpad and mouse that I didn't know
+    xinput set-prop $(xinput list --id-only 'ELAN Touchscreen') "Device Enabled" 0
+    xinput set-prop $(xinput list --id-only 'SYN1B7B:01 06CB:2969') "Device Enabled" 0
+    
 else
     xinput set-prop "$id" "Device Enabled" 1
     ./scripts/touchpad_on.sh
