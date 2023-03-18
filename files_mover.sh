5ccc5801cdccb287da6d69232b608d7da9f9b362
arisuvade <ariesdave253@gmail.com>
2023-03-18 14:04:46 +0800

    first commit

---
+++ files_mover.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dir_path="/home/aries/Downloads/"
+
+docs_path="/home/aries/Documents/"
+pics_path="/home/aries/Pictures/"
+
+for file in "$dir_path"*
+do
+    if [[ "$file" == *.png || "$file" == *.jpg ]]; then
+        echo "$(basename "$file") moved to Pictures."
+        mv "$file" "$pics_path"
+    elif [[ "$file" == *.pdf || "$file" == *.docx ]]; then
+        echo "$(basename "$file") moved to Documents."
+        mv "$file" "$docs_path"
+    fi
+done
+
