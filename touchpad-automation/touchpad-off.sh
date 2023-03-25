043e09c0ecc3b826a670839fc1550c692b9c00c3
arisuvade <ariesdave253@gmail.com>
2023-03-25 16:24:47 +0800

    Removed the output

--- touchpad-automation/touchpad-off.sh
+++ touchpad-automation/touchpad-off.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Off touchpad
-synclien TouchpadOff=1
+synclient TouchpadOff=1
 
 id=$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')
 xinput set-prop "$id" "Device Enabled" 0
-
-echo "Touchpad off"
--- touchpad-automation/touchpad-on.sh
+++ touchpad-automation/touchpad-on.sh
@@ -6,17 +6,12 @@ synclient TouchpadOff=0
 id=$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')
 xinput set-prop "$id" "Device Enabled" 1
 
-echo "Touchpad on"
-
 # On the tap-to-click
 synclient TapButton1=1
-echo "Tap-to-click on"
 
 # On the right click
 synclient TapButton2=3
-echo "Touchpad right click on"
 
 # On natural scrolling
 synclient VertScrollDelta=-111
 synclient HorizScrollDelta=-111
-echo "Natural scrolling on"
