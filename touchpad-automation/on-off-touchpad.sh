cefd7a6fcbd5f11e0e26cba6c361f6b63540873e
arisuvade <ariesdave253@gmail.com>
2023-03-25 16:25:00 +0800

    Initial commit

---
+++ touchpad-automation/on-off-touchpad.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Touchpad
+touchpad=$(xinput list-props "SYN1B7B:01 06CB:2969 Touchpad" | grep "Device Enabled" | awk '{print $4}')
+
+# Off
+if [[ $touchpad -eq 1 ]]; then
+    ~/Documents/scripts/touchpad-automation/touchpad-off.sh
+# On
+else
+    ~/Documents/scripts/touchpad-automation/touchpad-on.sh
+fi
