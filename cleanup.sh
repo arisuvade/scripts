ac409cd0c1a6503e33860813ccda7eaf93617fb7
arisuvade <ariesdave253@gmail.com>
2023-03-27 21:15:46 +0800

    Removed cleaner.sh

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install paccache if not already installed
-paccache="/usr/bin/paccache"
-[[ ! -f $paccache ]] && sudo pacman -S pacman-contrib
-
-# To clear cache
-sudo paccache -rk0
