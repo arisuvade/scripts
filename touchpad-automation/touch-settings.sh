a16339294bb3ac49e87cc99db283038c39c19a82
arisuvade <ariesdave253@gmail.com>
2023-03-29 08:21:25 +0800

    Initial commit

---
+++ touchpad-automation/touch-settings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+synclient LeftEdge=113
+synclient RightEdge=2719
+synclient TopEdge=127
+synclient BottomEdge=2237
+synclient FingerLow=2
+synclient FingerHigh=3
+synclient MaxTapTime=180
+synclient MaxTapMove=162
+synclient MaxDoubleTapTime=180
+synclient SingleTapTimeout=180
+synclient ClickTime=100
+synclient EmulateMidButtonTime=0
+synclient ClickTime=100
+synclient EmulateTwoFingerMinZ=56
+synclient EmulateTwoFingerMinW=7
+synclient VertEdgeScroll=0
+synclient HorizEdgeScroll=0
