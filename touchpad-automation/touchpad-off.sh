4dd809f3db511936c84913d8641bb030259b7c1c
arisuvade <ariesdave253@gmail.com>
2023-04-03 17:47:45 +0800

    Touchpad name move to variable

--- touchpad-automation/touchpad-off.sh
+++ touchpad-automation/touchpad-off.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Change to your touchpad name
+touchpad_name="SYN1B7B:01 06CB:2969 Touchpad"
+
 # Off touchpad
 synclient TouchpadOff=1
 
-id=$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')
+id=$(xinput list --id-only "$touchpad_name")
 xinput set-prop "$id" "Device Enabled" 0
