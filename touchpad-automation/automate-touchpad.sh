5f5e9fc4a68a71cead95affb13b52f9de6909829
arisuvade <ariesdave253@gmail.com>
2023-04-03 17:50:52 +0800

    Modified id's

--- touchpad-automation/automate-touchpad.sh
+++ touchpad-automation/automate-touchpad.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-id=$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')
+# Change to your touchpad name and id
+touchpad_name="SYN1B7B:01 06CB:2969 Touchpad"
+touchpad_id=13
 
-if [[ $id -ne 13 ]]; then
-    xinput set-prop "$id" "Device Enabled" 0
+current_id=$(xinput list --id-only "$touchpad_name")
+
+if [[ $current_id -ne $touchpad_id ]]; then
+    xinput set-prop "$current_id" "Device Enabled" 0
 
     # Disable touchpad and mouse that I didn't know
     xinput set-prop "$(xinput list --id-only 'ELAN Touchscreen')" "Device Enabled" 0
     xinput set-prop "$(xinput list --id-only 'SYN1B7B:01 06CB:2969')" "Device Enabled" 0
     
 else
-    xinput set-prop "$id" "Device Enabled" 1
+    xinput set-prop "$current_id" "Device Enabled" 1
     ./Documents/github-repos/scripts/touchpad-automation/touchpad-on.sh
 fi
