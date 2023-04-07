d4f7b8f8ad0dc954370ee22a0c6e0ba7cf1355c7
arisuvade <ariesdave253@gmail.com>
2023-04-07 14:09:54 +0800

    Make executable

--- touchscreen-automation/on-off-touchscreen.sh
+++ touchscreen-automation/on-off-touchscreen.sh
@@ -5,7 +5,7 @@ touchscreen_name="ELAN Touchscreen"
 
 # Touchscreen
 touchscreen=$(xinput list-props "$touchscreen_name" | grep "Device Enabled" | awk '{print $4}')
-
+uch
 # Off
 if [[ $touchscreen -eq 1 ]]; then
     ~/GitHub/scripts/touchscreen-automation/touchscreen-off.sh
