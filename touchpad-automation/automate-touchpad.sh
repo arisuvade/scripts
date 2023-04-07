be2eec5d02d396f3cb555ff2dcff2a89cf308163
arisuvade <ariesdave253@gmail.com>
2023-04-07 13:54:47 +0800

    Fix mouse bug

--- touchpad-automation/automate-touchpad.sh
+++ touchpad-automation/automate-touchpad.sh
@@ -9,9 +9,9 @@ current_id=$(xinput list --id-only "$touchpad_name")
 if [[ $current_id -ne $touchpad_id ]]; then
     xinput set-prop "$current_id" "Device Enabled" 0
 
-    # Disable touchpad and mouse that I didn't know
+    # Disable touchscreen and mouse that I didn't know
     xinput set-prop "$(xinput list --id-only 'ELAN Touchscreen')" "Device Enabled" 0
-    xinput set-prop "$(xinput list --id-only 'SYN1B7B:01 06CB:2969')" "Device Enabled" 0
+    xinput set-prop "$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Mouse')" "Device Enabled" 0
     
 else
     xinput set-prop "$current_id" "Device Enabled" 1
