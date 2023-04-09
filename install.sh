842a856df836032e4031c37886294e43447540be
arisuvade <ariesdave253@gmail.com>
2023-04-09 11:14:59 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# File operations
+cd file-operations && ./install.sh && cd ..
+
+# System checks
+cd system-checks && ./install.sh && cd ..
+
+# System controls
+cd system-controls && ./install.sh && cd ..
+
+# System error fixes
+cd system-error-fixes && ./install.sh && cd ..
+
+# Terminal utils
+cd terminal-utils && ./install.sh
