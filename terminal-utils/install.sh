20e111e1dadd8714bf0c6329cbe4be1b16e88238
arisuvade <ariesdave253@gmail.com>
2023-04-09 11:06:06 +0800

    Initial commit

---
+++ terminal-utils/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Help section
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "Usage:"
+    echo "  [OPTION] [VALUE]"
+    echo ""
+    echo "Commands:"
+    echo "  -h, --help      Show this help and exit"
+    echo ""
+    echo "Aliases:"
+    echo "  assign          Assign alias to the file"
+    echo "  makeit          Make the file executable"
+    exit 0
+fi
+
+# Add aliases to their respective shell
+case "$SHELL" in
+    "/bin/bash")
+        echo -e "\nalias assign='~/scripts/terminal-utils/assign-alias.sh'" >> ~/.bashrc
+        echo "alias makeit='~/scripts/terminal-utils/make-it-executable.sh'" >> ~/.bashrc
+        ;;
+    "/usr/bin/zsh")
+        echo -e "\nalias assign='~/scripts/terminal-utils/assign-alias.sh'" >> ~/.zshrc
+        echo "alias makeit='~/scripts/terminal-utils/make-it-executable.sh'" >> ~/.zshrc
+        ;;
+    *)
+        echo "Error: Shell not found!"
+        exit 1
+esac
