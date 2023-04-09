11a0b449693d6927276d50da15e42e5754b42331
arisuvade <ariesdave253@gmail.com>
2023-04-09 12:19:00 +0800

    Add to shell

--- update-scheduler.sh
+++ update-scheduler.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+case "$SHELL" in
+    "/bin/bash")
+        echo -e "# Update scheduler" >> .bashrc
+        echo "~/scripts/update-scheduler.sh" >> ~/.bashrc
+        ;;
+    "/usr/bin/zsh")
+        echo -e "# Update scheduler" >> .zshrc
+        echo "~/scripts/update-scheduler.sh" >> ~/.zshrc
+        ;;
+    *)
+        echo "Error: Shell not found!"
+        exit 1
+
 day=$(date | awk '{print $1}')
 [[ $day == "Fri"  ]] && echo "It's Friday. Time to update your system!!!"
