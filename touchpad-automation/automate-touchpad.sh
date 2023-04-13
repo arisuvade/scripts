07a04ed2bf5c8136b270d3e5c20837ed1022c91d
arisuvade <ariesdave253@gmail.com>
2023-04-13 19:39:28 +0800

    Add mouse pointer speed

--- touchpad-automation/automate-touchpad.sh
+++ touchpad-automation/automate-touchpad.sh
@@ -6,13 +6,18 @@ touchpad_id=13
 
 current_id=$(xinput list --id-only "$touchpad_name")
 
+# Off touchpad and on mouse pointer speed
 if [[ $current_id -ne $touchpad_id ]]; then
     xinput set-prop "$current_id" "Device Enabled" 0
 
     # Disable touchscreen and mouse that I didn't know
     xinput set-prop "$(xinput list --id-only 'ELAN Touchscreen')" "Device Enabled" 0
     xinput set-prop "$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Mouse')" "Device Enabled" 0
-    
+
+    # On mouse pointer speed
+    ~/GitHub/scripts/mouse-pointer-speed.sh
+
+# On touchpad
 else
     xinput set-prop "$current_id" "Device Enabled" 1
     ~/GitHub/scripts/touchpad-automation/touchpad-on.sh
