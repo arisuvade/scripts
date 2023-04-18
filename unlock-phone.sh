df510c9cfce8ab9d4cec2259ea3c7a654b8b1885
arisuvade <ariesdave253@gmail.com>
2023-04-18 15:37:42 +0800

    Remove hard coded pass

--- unlock-phone.sh
+++ unlock-phone.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+read -r -p "Enter your passcode: " PASS
+
 # Open phone
 adb shell input keyevent KEYCODE_WAKEUP
 
 # Type password and enter
-adb shell input text "0301"
+adb shell input text "$PASS"
 adb shell input keyevent KEYCODE_ENTER
