066755964346661a5c240a93a4b41ed9ef1a0424
arisuvade <ariesdave253@gmail.com>
2023-04-27 14:47:52 +0800

    Add other scripts

--- install.sh
+++ install.sh
@@ -15,5 +15,25 @@ cd system-error-fixes && ./install.sh && cd ..
 # Terminal utils
 cd terminal-utils && ./install.sh && cd ..
 
-# Update scheduler
-./update-scheduler.sh
+# Other scripts
+case "$SHELL" in
+    "/bin/bash")
+        echo -e "\n# Other scripts" ~/.bashrc
+        echo "alias anime='~/scripts/anime_checker.py'" >> ~/.bashrc
+        echo "alias mouse='~/scripts/mouse-pointer-speed.sh'" >> ~/.bashrc
+        echo "alias unlockp='~/scripts/unlock-phone.sh'" >> ~/.bashrc
+        echo "alias updatesched='~/scripts/update-scheduler.sh'" >> ~/.bashrc
+        echo "alias videor='~/scripts/video-reflectors.sh'" >> ~/.bashrc
+        ;;
+    "/usr/bin/zsh")
+        echo -e "\n# Other scripts" ~/.zshrc
+        echo "alias anime='~/scripts/anime_checker.py'" >> ~/.zshrc
+        echo "alias mouse='~/scripts/mouse-pointer-speed.sh'" >> ~/.zshrc
+        echo "alias unlockp='~/scripts/unlock-phone.sh'" >> ~/.zshrc
+        echo "alias updatesched='~/scripts/update-scheduler.sh'" >> ~/.zshrc
+        echo "alias videor='~/scripts/video-reflectors.sh'" >> ~/.zshrc
+        ;;
+    *)
+        echo "Error: Shell not found!"
+        exit 1
+esac
