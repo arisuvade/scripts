86491c50cdc7db89176b9b0bfb43b65d9ee56b8f
arisuvade <ariesdave253@gmail.com>
2023-05-04 22:09:19 +0800

    Remove unnecessary scripts

--- video-reflectors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Open 2 browsers with white background
-brave -disk-cache-dir=Documents --disk-cache-size=104857600 --new-window https://www.drodd.com/images14/white1.jpg
-brave -disk-cache-dir=Documents --disk-cache-size=104857600 --new-window https://www.drodd.com/images14/white1.jpg
-
-# Exit terminal
-killall alacritty
