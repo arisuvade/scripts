366ed3d2fcff2efadb013e48e564e6d26a116436
arisuvade <ariesdave253@gmail.com>
2023-05-29 21:33:10 +0800

    Remove one option

--- file-operations/cleanup.sh
+++ file-operations/cleanup.sh
@@ -2,19 +2,13 @@
 
 option=$1
 
-if [[ "$option" == "cleanup" ]]; then
-    sudo pacman -Scc
-
-elif [[ "$option" == "paccache" ]]; then
-
+if [[ "$option" == "paccache" ]]; then
     # Install paccache if not already installed
     paccache="/usr/bin/paccache"
     [[ ! -f $paccache ]] && sudo pacman -S pacman-contrib
 
     # To clear cache
     sudo paccache -rk0
-
 else
-    echo "Invalid option"
-    exit 1
+    sudo pacman -Scc
 fi
