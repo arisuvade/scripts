9e6ce72eebac92dab948349662b675f364b0c59d
arisuvade <ariesdave253@gmail.com>
2023-05-29 21:33:59 +0800

    Restructure

--- file-operations/move-file.sh
+++ file-operations/move-file.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 dir_path="/home/aries/Downloads/"
-
 docs_path="/home/aries/Documents/"
 pics_path="/home/aries/Pictures/"
 
-for file in "$dir_path"*
-do
+for file in "$dir_path"*; do
     if [[ "$file" == *.png || "$file" == *.jpg ]]; then
         echo "$(basename "$file") moved to Pictures."
         mv "$file" "$pics_path"
@@ -15,4 +13,3 @@ do
         mv "$file" "$docs_path"
     fi
 done
-
