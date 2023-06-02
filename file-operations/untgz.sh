758c844312ca629b77d05d76c656544bbe115fa8
arisuvade <ariesdave253@gmail.com>
2023-06-02 19:56:43 +0800

    Add rules

--- file-operations/untgz.sh
+++ file-operations/untgz.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-tar -xvzf $1
+file="$1"
+
+# Check if the argument is not provided
+if [ -z "$file" ]; then
+  echo "No file provided."
+  exit 1
+fi
+
+# Check if the argument is not a file
+if [ ! -f "$file" ]; then
+  echo "$file is not a file."
+  exit 1
+fi
+
+# Check if the file is not a tar file
+if ! file "$file" | grep -q "tar archive"; then
+  echo "$file is not a valid tar file."
+  exit 1
+fi
+
+# Extract the tar file
+tar -xvzf "$file"
