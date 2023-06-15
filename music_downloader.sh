b9428040a29e589389424a22dd836fac64eef4b3
arisuvade <ariesdave253@gmail.com>
2023-06-15 14:02:09 +0800

    Initial commit

---
+++ music_downloader.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while true; do
+    read -p "URL: " url
+    if [[  $url =~ ^https://youtu\.be/[a-zA-Z0-9_-]{11}$ ]]; then
+        break
+    else
+        echo -e "Invalid YouTube URL. To obtain the proper URL, go to the YouTube video you want to download and click on the 'Share' button below the video player. Then, select the 'Copy' option to get the URL. Ensure the copied URL has the format 'https://youtu.be/xxxxxxxxxxx'.\n"
+    fi
+done
+
+read -p "Filename (Enter for default): " filename
+
+if [[ -z $filename ]]; then
+    output=$(yt-dlp -x --audio-format mp3 $url)
+else
+    output=$(yt-dlp -x --audio-format mp3 -o "${filename}.%(ext)s" "$url" 2>&1)
+fi
+
+if [[ $output == *"ERROR"* ]]; then
+    echo "Error occurred while downloading the music."
+else
+    echo "Download complete."
+fi
