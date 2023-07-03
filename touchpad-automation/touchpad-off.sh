e0f817ab8b8950d472423420e2f1b8e73a6c780a
arisuvade <ariesdave253@gmail.com>
2023-07-03 16:14:24 +0800

    Remove synclient

--- touchpad-automation/touchpad-off.sh
+++ touchpad-automation/touchpad-off.sh
@@ -4,7 +4,7 @@
 touchpad_name="SYN1B7B:01 06CB:2969 Touchpad"
 
 # Off touchpad
-synclient TouchpadOff=1
+# synclient TouchpadOff=1
 
 id=$(xinput list --id-only "$touchpad_name")
 xinput set-prop "$id" "Device Enabled" 0
