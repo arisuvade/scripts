c704a40bd80d1ad31a9f6ef7697c720d302ea10b
arisuvade <ariesdave253@gmail.com>
2023-07-09 09:30:17 +0800

    Bring back synaptic setting

--- touchpad-automation/touchpad-on.sh
+++ touchpad-automation/touchpad-on.sh
@@ -4,20 +4,20 @@
 touchpad_name="SYN1B7B:01 06CB:2969 Touchpad"
 
 # On the touchpad
-# synclient TouchpadOff=0
+synclient TouchpadOff=0
 
 id=$(xinput list --id-only "$touchpad_name")
 xinput set-prop "$id" "Device Enabled" 1
 
 # On the tap-to-click
-# synclient TapButton1=1
+synclient TapButton1=1
 
 # On the right click
-# synclient TapButton2=3
+synclient TapButton2=3
 
 # On natural scrolling
-# synclient VertScrollDelta=-111
-# synclient HorizScrollDelta=-111
+synclient VertScrollDelta=-111
+synclient HorizScrollDelta=-111
 
 # On touchpad settings
-# ~/GitHub/scripts/touchpad-automation/touch-settings.sh
+~/GitHub/scripts/touchpad-automation/touch-settings.sh
