0884e74cd1f7cf07326dc8dae6f475b29bc2788b
arisuvade <ariesdave253@gmail.com>
2023-07-15 20:27:00 +0800

    Add yt-dlp checker

--- yt-music-dl.sh
+++ yt-music-dl.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if ! command -v yt-dlp &> /dev/null; then
+    echo "yt-dlp is not installed. Please install it before running this script."
+    exit 1
+fi
+
 while true; do
     read -p "URL: " url
-    if [[  $url =~ ^https://youtu\.be/[a-zA-Z0-9_-]{11}$ ]]; then
+    if [[ $url =~ ^https://youtu\.be/[a-zA-Z0-9_-]{11}$ ]]; then
         break
     else
         echo -e "Invalid YouTube URL. To obtain the proper URL, go to the YouTube video you want to download and click on the 'Share' button below the video player. Then, select the 'Copy' option to get the URL. Ensure the copied URL has the format 'https://youtu.be/xxxxxxxxxxx'.\n"
