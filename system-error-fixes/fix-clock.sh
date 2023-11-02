92a9848322b2c9a960deb102c52725fac7d7e73a
arisuvade <ariesdave253@gmail.com>
2023-11-02 10:36:32 +0800

    Remove online mode not working

--- system-error-fixes/fix-clock.sh
+++ system-error-fixes/fix-clock.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Check whether between online or offline
-if [ $# -eq 2 ]; then
-    # Online mode
-    current_date=$(date -u -d "$(curl -s --head 'https://www.google.com' | grep ^Date: | sed 's/Date: //')" +"%Y-%m-%d")
-    new_datetime="$current_date $1:$2"
-
-elif [ $# -eq 4 ]; then
-    # Offline mode
+# Set and check if the input is correct
+if [ $# -eq 4 ]; then
     new_datetime="2023-$1-$2 $3:$4"
-
 else
-    echo "Usage: clock (month day) hour minute"
+    echo "Usage: clock month day hour minute"
     exit 1
 fi
 
