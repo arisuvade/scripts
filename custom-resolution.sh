f95ed9655729ceb8c8644a273ec4f2c9c8a6c609
arisuvade <ariesdave253@gmail.com>
2024-02-25 11:16:15 +0800

    Add gtf

--- custom-resolution.sh
+++ custom-resolution.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+gtf 1434 805 60.06
 xrandr --newmode "1432x805_60.06"  94.17  1432 1504 1656 1880  805 806 809 834  -HSync +Vsync
 xrandr --addmode eDP-1 "1432x805_60.06"
 xrandr --output eDP-1 --mode "1432x805_60.06"
