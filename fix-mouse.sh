982c267b378e1c76f84a8510112756da19a62da5
arisuvade <ariesdave253@gmail.com>
2024-03-04 01:08:57 +0800

    Fix touchpad not off because of mouse

--- fix-mouse.sh
+++ fix-mouse.sh
@@ -7,4 +7,4 @@ xinput --set-prop $mouse_id 'libinput Accel Speed' -0.9
 
 # Off touchpad and touchscreen
 xinput set-prop "$(xinput list --id-only 'ELAN Touchscreen')" "Device Enabled" 0
-xinput set-prop "$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Mouse')" "Device Enabled" 0
+xinput set-prop "$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Touchpad')" "Device Enabled" 0
