c60867abaf6d8d235a07b2d2410ae4176f4837a2
arisuvade <ariesdave253@gmail.com>
2024-03-04 14:30:29 +0800

    Change reso

--- custom-resolution.sh
+++ custom-resolution.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gtf 1434 805 60.06
-xrandr --newmode "1432x805_60.06"  94.17  1432 1504 1656 1880  805 806 809 834  -HSync +Vsync
-xrandr --addmode eDP-1 "1432x805_60.06"
-xrandr --output eDP-1 --mode "1432x805_60.06"
+gtf 1461 823 60.06
+xrandr --newmode "1464x823_60.06"  98.66  1464 1544 1696 1928  823 824 827 852  -HSync +Vsync
+xrandr --addmode eDP-1 "1464x823_60.06"
+xrandr --output eDP-1 --mode "1464x823_60.06"
