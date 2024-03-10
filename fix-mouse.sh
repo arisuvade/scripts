fca2f6f1592fa49e2686a9d86a694d0370c3f694
arisuvade <ariesdave253@gmail.com>
2024-03-10 23:47:31 +0800

    Add off touchpad

--- fix-mouse.sh
+++ fix-mouse.sh
@@ -2,4 +2,8 @@
 
 mouse_id=$(xinput list | grep -m 1 "2\.4G Mouse" | grep -oP 'id=\K\d+')
 
+# Set mouse speed
 xinput --set-prop $mouse_id 'libinput Accel Speed' -0.9
+
+# Off the touchpad
+~/github/scripts/touchpad-automation/touchpad-off.sh
