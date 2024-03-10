8d3b18283ee79e8673c17d8d6f5938b06eac9152
arisuvade <ariesdave253@gmail.com>
2024-03-11 00:06:04 +0800

    Remove

--- mouse-pointer-speed.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mouse_id=$(xinput list --id-only 'pointer:2.4G Mouse')
-
-# Set mouse speed
-xinput --set-prop $mouse_id 'libinput Accel Speed' -0.9
-
--- open-website.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Check if Browser is running
-if pgrep -x "Thorium-Browser" > /dev/null; then
-    # If Browser is running, open the website in a new tab
-    ~/Documents/Thorium-Browser-AVX2.AppImage --new-tab $1
-fi
