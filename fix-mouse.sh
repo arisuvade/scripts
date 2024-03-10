15d82706f58a97c8d8f2d8d93d39daaac2657e09
arisuvade <ariesdave253@gmail.com>
2024-03-10 23:55:37 +0800

    Improve touchpad detection and add touchscreen

--- fix-mouse.sh
+++ fix-mouse.sh
@@ -5,5 +5,6 @@ mouse_id=$(xinput list --id-only 'pointer:2.4G Mouse')
 # Set mouse speed
 xinput --set-prop $mouse_id 'libinput Accel Speed' -0.9
 
-# Off the touchpad
-~/github/scripts/touchpad-automation/touchpad-off.sh
+# Off touchpad and touchscreen
+xinput set-prop "$(xinput list --id-only 'ELAN Touchscreen')" "Device Enabled" 0
+xinput set-prop "$(xinput list --id-only 'SYN1B7B:01 06CB:2969 Mouse')" "Device Enabled" 0
