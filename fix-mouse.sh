95c381166fa3df9c6e119e06eccc0e3ded4de666
arisuvade <ariesdave253@gmail.com>
2024-03-10 23:54:26 +0800

    Improve mouse id detection

--- fix-mouse.sh
+++ fix-mouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mouse_id=$(xinput list | grep -m 1 "2\.4G Mouse" | grep -oP 'id=\K\d+')
+mouse_id=$(xinput list --id-only 'pointer:2.4G Mouse')
 
 # Set mouse speed
 xinput --set-prop $mouse_id 'libinput Accel Speed' -0.9
