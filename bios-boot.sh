9088872aacab1f6ecd6cae9e9ea84b7bf8102b65
arisuvade <ariesdave253@gmail.com>
2024-04-08 17:54:29 +0800

    Add shebang and comments

--- bios-boot.sh
+++ bios-boot.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 if [[ "$1" == "off" ]]; then
+    # Not boot in BIOS
     bootctl reboot-to-firmware false
     echo "Booting to BIOS disabled!"
 else
+    # Boot in BIOS
     bootctl reboot-to-firmware true
     echo "Booting to BIOS enabled!"
 fi
